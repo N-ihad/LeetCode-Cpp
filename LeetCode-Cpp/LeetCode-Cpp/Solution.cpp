7f788bbb5bce157b1ae50128059067ba73859318
Nihad Samedov <the_nihad@icloud.com>
2021-11-21 21:12:49 +0300

    Add solution

--- LeetCode-Cpp/LeetCode-Cpp/Solution.cpp
+++ LeetCode-Cpp/LeetCode-Cpp/Solution.cpp
@@ -14,15 +14,35 @@ using namespace std;
 
 class Solution {
 public:
-
+    void reverseString(vector<char>& s) {
+        for(int i=0; i<s.size() / 2; ++i) {
+            swap(s[i], s[s.size() - 1 - i]);
+        }
+    }
 };
 
-TEST_CASE("example") {
+TEST_CASE("hello") {
     // given
     Solution sut;
+    vector<char> str{'h', 'e', 'l', 'l', 'o'};
 
     // when
+    sut.reverseString(str);
 
     // then
-    REQUIRE(1 == 1);
+    vector<char> expectedResult{'o', 'l', 'l', 'e', 'h'};
+    REQUIRE(str == expectedResult);
+}
+
+TEST_CASE("Hannah") {
+    // given
+    Solution sut;
+    vector<char> str{'H', 'a', 'n', 'n', 'a', 'h'};
+
+    // when
+    sut.reverseString(str);
+
+    // then
+    vector<char> expectedResult{'h', 'a', 'n', 'n', 'a', 'H'};
+    REQUIRE(str == expectedResult);
 }
