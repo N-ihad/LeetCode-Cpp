6fe4e4e547553b918add926a339717ff2818a6ff
Nihad Samedov <the_nihad@icloud.com>
2021-11-21 21:07:14 +0300

    Add testing bundle

---
+++ LeetCode-Cpp/LeetCode-Cpp/Solution.cpp
@@ -0,0 +1,28 @@
+//
+//  Solution.cpp
+//  LeetCode-Cpp
+//
+//  Created by Nihad on 11/21/21.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include <vector>
+#include "catch.hpp"
+
+using namespace std;
+
+class Solution {
+public:
+
+};
+
+TEST_CASE("example") {
+    // given
+    Solution sut;
+
+    // when
+
+    // then
+    REQUIRE(1 == 1);
+}
--- LeetCode-Cpp/LeetCode-Cpp/main.cpp
+++ LeetCode-Cpp/LeetCode-Cpp/main.cpp
@@ -5,10 +5,4 @@
 //  Created by Nihad on 11/21/21.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+#include "Solution.cpp"
