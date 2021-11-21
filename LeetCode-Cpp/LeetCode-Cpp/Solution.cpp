938136e6481eb9849143d4b829cf8168f0f29c3c
Nihad Samedov <the_nihad@icloud.com>
2021-11-21 22:29:08 +0300

    Solution

--- LeetCode-Cpp/LeetCode-Cpp/Solution.cpp
+++ LeetCode-Cpp/LeetCode-Cpp/Solution.cpp
@@ -14,15 +14,65 @@ using namespace std;
 
 class Solution {
 public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return {nums};
+        }
 
+        vector<vector<int>> result;
+
+        for(int i=0; i<nums.size(); ++i) {
+            vector<int> v(nums.begin(), nums.end());
+            v.erase(v.begin() + i);
+            auto res = permute(v);
+
+            for(int j=0; j<res.size(); ++j) {
+                vector<int> _v = res[j];
+                _v.insert(_v.begin(), nums[i]);
+                result.push_back(_v);
+            }
+        }
+
+        return result;
+    }
 };
 
-TEST_CASE("example") {
+TEST_CASE("[1, 2, 3]") {
     // given
     Solution sut;
+    vector<int> nums{1, 2, 3};
 
     // when
+    vector<vector<int>> result = sut.permute(nums);
 
     // then
-    REQUIRE(1 == 1);
+    vector<vector<int>> expectedResult = {{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}};
+    REQUIRE(result == expectedResult);
 }
+
+TEST_CASE("[0, 1]") {
+    // given
+    Solution sut;
+    vector<int> nums{0, 1};
+
+    // when
+    vector<vector<int>> result = sut.permute(nums);
+
+    // then
+    vector<vector<int>> expectedResult = {{0, 1}, {1, 0}};
+    REQUIRE(result == expectedResult);
+}
+
+TEST_CASE("[1]") {
+    // given
+    Solution sut;
+    vector<int> nums{1};
+
+    // when
+    vector<vector<int>> result = sut.permute(nums);
+
+    // then
+    vector<vector<int>> expectedResult = {{1}};
+    REQUIRE(result == expectedResult);
+}
+
